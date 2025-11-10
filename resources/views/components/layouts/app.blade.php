bc12b827a669c180c71d8926b150431f43412ccf
qureshiweb <qureshiweb4@gmail.com>
2025-11-10 22:12:47 +0500

    template layout setup

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Job - Marketplace</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/theme/favicon.svg')}}" />
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/animate.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/main.css?v=1.0')}}" />
+</head>
+
+<body>
+    
+    @include('partials.header')
+
+    {{ $slot }}
+
+    @include('partials.footer')
+
+
+    <script src="{{asset('assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/waypoints.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/wow.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/magnific-popup.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/select2.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/isotope.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/scrollup.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/swiper-bundle.min.js')}}"></script>
+    <!-- Template  JS -->
+    <script src="{{asset('assets/js/main.js?v=1.0')}}"></script>
+</body>
+
+</html>
